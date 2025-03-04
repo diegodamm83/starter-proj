cadfd368aa4748ebb14c6fe43fe306272223fee3
Ultimate553 <diego.torred@gmail.com>
2025-03-04 00:15:12 -0600

    changed ports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,5 +17,5 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3001");
 });
